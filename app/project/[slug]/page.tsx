9be1b166f173c437aa37dfb5e8818dfd53f665d1
taoguangc <taoguangc@gmail.com>
2023-11-10 18:43:58 +0800

    fix project/[slug]/page.tsx

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getProject } from '@/sanity/lib/queries
+import { getProject } from '@/sanity/lib/queries'
 import { PortableText } from '@portabletext/react'
 import Image from 'next/image'
 import { getImageDimensions } from '@sanity/asset-utils'
